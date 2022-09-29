4a658435f78c58ff8baba2ddb2c49d4cafa02bae
Nagehan Uyumaz <uyumaznagehan@gmail.com>
2022-09-29 01:53:43 +0100

    exercises 1

--- exercise.js
+++ exercise.js
@@ -15,15 +15,31 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let element =document.querySelectorAll("p")
+console.log(element)
 
+let elements =document.querySelector("div")
+console.log(element)
+
+let jumbotron=document.querySelector("#jumbotron-text")
+console.log(jumbotron)
+
+let pElements= document.querySelector(".primary-content")
+const p= pElements.querySelectorAll("p")
+console.log(p)
 
 /*
 Task 2
 ======
+ alertButton.addEventL
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
+let alertButton = document.querySelector("#alertBtn");
+function alertF(){
+    alert("Thanks for visiting Bikes for Refugees")
+}
+alertButton.addEventListener("click", alertF);
 
 /*
 Task 3
@@ -31,20 +47,35 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
-
+let changeColorBtn = document.getElementById("bgrChangeBtn");
+let body = document.querySelector("body");
+changeColorBtn.addEventListener("click", () => {
+body.style.backgroundColor = "pink";
+}); 
 /*
+
+
 Task 4
 ======
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+let addText = document.getElementById("addTextBtn");
+let JumTxt = document.querySelector(".jumbotron");
+addText.addEventListener("click",()=>{
+    const newP = document.createElement("p");
+    newP.innerText =" oku beni"
+    JumTxt.appendChild(newP)
+})   
 
 
-
-/*
+/*All
 Task 5
 ======
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+let addLargerLinks = document.getElementById("largersLinksBtn")
+let allLinks =document.querySelectorAll("a");
+
